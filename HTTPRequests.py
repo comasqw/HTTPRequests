9f580a8b379ee740456ae1a745aa0ec5b33ba55f
comasqw <arizona.rp.91@mail.ru>
2024-10-17 23:54:01 +0400

    add base http client & response parser

--- HTTPRequests.py
+++ HTTPRequests.py
@@ -1,4 +1,6 @@
 import json
+import socket
+
 
 INDENT = "\r\n"
 
@@ -57,6 +59,7 @@ class HTTPRequest:
                  query_string: dict[str, str] | None = None,
                  body: str | dict | None = None):
 
+        self.url = url
         self.hostname, self.path = url_parse(url)
         self.method = method.upper() if method else HTTPMethods.GET
         self.request_headers = request_headers if request_headers else base_request_headers
@@ -65,14 +68,14 @@ class HTTPRequest:
         self.body = body
         self.request = self.create_request_str()
 
-    def _create_request_start_line_str(self):
+    def _create_request_start_line_str(self) -> str:
         path = self.path
         if self.query_string:
             path += "?" + "&".join([f"{key}={value}" for key, value in self.query_string.items()])
         request_start_line = f"{self.method} {path} {self.http_version}{INDENT}"
         return request_start_line
 
-    def _create_request_headers_str(self):
+    def _create_request_headers_str(self) -> str:
         request_headers_str = f"{HTTPHeaders.HOST}: {self.hostname}{INDENT}"
         if self.body and self.method == HTTPMethods.POST:
             content_type = ContentTypes.TEXT
@@ -86,7 +89,7 @@ class HTTPRequest:
 
         return request_headers_str
 
-    def create_request_str(self):
+    def create_request_str(self) -> str:
         request_start_line_str = self._create_request_start_line_str()
         request_headers_str = self._create_request_headers_str()
         request_str = request_start_line_str + request_headers_str + INDENT + INDENT
@@ -97,3 +100,44 @@ class HTTPRequest:
                 body_str = str(self.body)
             request_str += body_str
         return request_str
+
+
+class HTTPResponse:
+    def __init__(self, response: str):
+        self.response = response
+        self.http_version: str | None = None
+        self.status_code: int | None = None
+        self.headers = {}
+        self.body: str | None = None
+        self._parse_response()
+
+    def _parse_response(self):
+        response_header, response_body = self.response.split(INDENT + INDENT)
+        self.body = response_body
+
+        response_header_lines = response_header.split(INDENT)
+        http_version, status_code, *_ = response_header_lines[0].split()
+
+        self.http_version = http_version
+        self.status_code = int(status_code)
+
+        for line in response_header_lines[1:]:
+            header, *value = line.split(":")
+            self.headers[header] = "".join(value)
+
+
+class HTTPClient:
+
+    @staticmethod
+    def _connect_and_send_request(sock_client: socket.socket, host: tuple[str, int], data: str):
+        sock_client.connect(host)
+        sock_client.send(data.encode())
+
+        response = sock_client.recv(4096)
+        return response.decode()
+
+    def send_request(self, http_request: HTTPRequest, port: int = 80) -> HTTPResponse:
+        with socket.socket() as sock_client:
+            response = self._connect_and_send_request(sock_client, (http_request.hostname, port), http_request.request)
+            http_response = HTTPResponse(response)
+            return http_response
